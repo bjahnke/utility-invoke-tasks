3172d3aa5fab935aa9c5c0c7759c40bd4853b624
bjahnke <bjahnke71@gmail.com>
2023-06-09 15:04:17 -0400

    update tasks, bumped version

--- src/util_invoke_tasks/task.py
+++ src/util_invoke_tasks/task.py
@@ -1,3 +1,4 @@
+import pathlib
 from invoke import task
 import subprocess
 import yaml
@@ -166,3 +167,50 @@ def envtoyaml(c):
         yaml.dump(env_dict, file)
 
     print(f"YAML file '{yaml_file}' created successfully.")
+
+
+@task
+def buildenvpy(c):
+    """
+    Requires: .env in the root directory
+    Build the env_auto.py file from the variables in .env using this pattern:
+    import os and dotenv at the top of the file
+    include new line, then:
+    for each line in .env, if the line is not empty and does not start with '#', then
+    split the line on '=' and use the variable to write a line in env_auto.py:
+    <variable> = os.environ.get('<variable>')
+    :param c:
+    :return:
+    """
+    with open('.env', 'r') as f:
+        lines = f.readlines()
+    pathlib.Path('../env').mkdir(parents=True, exist_ok=True)
+    with open('../env/env_auto.py', 'w') as f:
+        f.write(
+            '"""\n'
+            'Desc:\n'
+            'env_auto.py is generated from .env by the `invoke buildenvpy` task.\n'
+            'it\'s purpose is to provide IDE support for environment variables.\n'
+            '"""\n'
+            '\n'
+            'import os\n'
+            'from dotenv import load_dotenv\n'
+            'load_dotenv()\n\n'
+            '\n'
+        )
+        for line in lines:
+            if line != '\n' and not line.startswith('#'):
+                variable = line.split('=')[0]
+                f.write(f'{variable} = os.environ.get(\'{variable}\')\n')
+    print('env/env_auto.py built from .env')
+
+
+@task
+def dockerrun(c):
+    """
+    run the docker image with IMAGE_NAME and .env variables
+    :param c:
+    :return:
+    """
+    print("Running docker image...")
+    subprocess.run(["docker", "run", "--env-file", ".env", get_env_var('IMAGE_NAME')])
