56cd20b88c96f2f9e33bdb284dd7de4a5250bc21
bjahnke <bjahnke71@gmail.com>
2023-06-09 15:18:09 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='utility-invoke-tasks',
-    version='0.0.5',
+    version='0.0.6',
     packages=['src.util_invoke_tasks'],
     url='',
     license='MIT',
